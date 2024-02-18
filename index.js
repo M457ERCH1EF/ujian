e5a8203efc6e0b02cd108fc0359e81c9fb0f5dba
M457ERCH1EF <57579708+M457ERCH1EF@users.noreply.github.com>
2024-02-18 07:17:57 +0000

    add REST API

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world!!")
+// create an Express REST API server that return JSON data i.e "Salam Malaysia Madani!!" when a user sends a GET request to the server's /api/ path
+
+const express = require('express');
+const app = express();
+
+app.get('/api/', (req, res) => {
+    res.json({ message: 'Salam Malaysia Madani' });
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+
+);
